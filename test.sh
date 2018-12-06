440f6b06c7625e5023c47bc302f7f5a90290414f
nrequeno <ppnacho85@gmail.com>
2018-12-06 16:03:10 +0100

    Updates in install and test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #pip2 uninstall ParetoLib
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 python setup.py clean --all
 python setup.py build
 python setup.py install --force --user
