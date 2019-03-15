9e9192dd5c5e56b1b31676a9269d2ef4c939b5f7
nrequeno <ppnacho85@gmail.com>
2019-03-15 15:27:24 +0100

    New coverages

--- Tests/coverage_all.sh
+++ Tests/coverage_all.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-python -m coverage run --parallel-mode test_Oracle_OraclePoint.py
-python -m coverage run --parallel-mode test_Oracle_OracleFunction.py
-python -m coverage run --parallel-mode test_Oracle_OracleSTL.py
-python -m coverage run --parallel-mode test_Oracle_OracleSTLe.py
-python -m coverage run --parallel-mode test_Geometry_Point.py
-python -m coverage run --parallel-mode test_Geometry_Rectangle.py
-python -m coverage run --parallel-mode test_Search_ResultSet.py
-python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleFunctionTestCase.test_2D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleFunctionTestCase.test_3D
-python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOraclePointTestCase.test_2D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOraclePointTestCase.test_3D
-python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLTestCase.test_1D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLTestCase.test_2D
-python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLeTestCase.test_1D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLeTestCase.test_2D
-python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLeLibTestCase.test_1D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py SearchOracleSTLeLibTestCase.test_2D
-#python -m coverage run --parallel-mode --concurrency=multiprocessing test_Search.py
-python -m coverage combine
-python -m coverage report
-#python -m coverage html -d coverage/report-python
-python -m coverage erase
+coverage run -m --parallel-mode pytest test_Oracle_OraclePoint.py
+coverage run -m --parallel-mode pytest test_Oracle_OracleFunction.py
+coverage run -m --parallel-mode pytest test_Oracle_OracleSTL.py
+coverage run -m --parallel-mode pytest test_Oracle_OracleSTLe.py
+coverage run -m --parallel-mode pytest test_Geometry_Point.py
+coverage run -m --parallel-mode pytest test_Geometry_Rectangle.py
+coverage run -m --parallel-mode pytest test_Search_ResultSet.py
+coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleFunctionTestCase::test_2D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleFunctionTestCase::test_3D
+coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOraclePointTestCase::test_2D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOraclePointTestCase::test_3D
+coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLTestCase::test_1D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLTestCase::test_2D
+coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLeTestCase::test_1D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLeTestCase::test_2D
+coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLeLibTestCase::test_1D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py::SearchOracleSTLeLibTestCase::test_2D
+#coverage run -m --parallel-mode --concurrency=multiprocessing pytest test_Search.py
+coverage combine
+coverage report
+#coverage html -d coverage/report-python
+coverage erase
